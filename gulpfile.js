7df4351fa013d1bd4dd757d42687cef4dc8532f7
z8Dimon8z <dima-lysak@yandex.ru>
2022-02-11 11:22:06 +0300

    реализованы selectы

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ function scripts() { // сжатие JS файлов и  подключение 
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'app/js/main.js' // подключен main.js
   ])
     .pipe(concat('main.min.js'))
