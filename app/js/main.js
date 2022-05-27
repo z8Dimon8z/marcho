8d14507d02185c6f86e1f8a25929ef6f29c69fba
Dima Lysak <60256445+z8Dimon8z@users.noreply.github.com>
2022-05-27 12:23:13 +0300

    почти финал

--- app/js/main.js
+++ app/js/main.js
@@ -1,126 +1,142 @@
 $(function () {
 
-  $('.blog-page__slider').slick({
-    prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="7px" height="14px" viewBox="0 0 7 14" version="1.1"><g><path d="M 5.25 12.25 C 5.027344 12.25 4.800781 12.164062 4.632812 11.992188 L 0.257812 7.617188 C -0.0859375 7.277344 -0.0859375 6.722656 0.257812 6.382812 L 4.632812 2.007812 C 4.972656 1.664062 5.527344 1.664062 5.867188 2.007812 C 6.210938 2.347656 6.210938 2.902344 5.867188 3.242188 L 2.113281 7 L 5.871094 10.757812 C 6.210938 11.097656 6.210938 11.652344 5.871094 11.996094 C 5.699219 12.164062 5.472656 12.25 5.25 12.25 Z M 5.25 12.25 "/></g></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="7px" height="14px" viewBox="0 0 7 14" version="1.1"><g><path d="M 1.75 12.25 C 1.527344 12.25 1.300781 12.164062 1.132812 11.992188 C 0.789062 11.652344 0.789062 11.097656 1.132812 10.757812 L 4.890625 7 L 1.132812 3.242188 C 0.789062 2.902344 0.789062 2.347656 1.132812 2.003906 C 1.472656 1.664062 2.027344 1.664062 2.367188 2.003906 L 6.742188 6.378906 C 7.085938 6.722656 7.085938 7.277344 6.742188 7.617188 L 2.367188 11.992188 C 2.199219 12.164062 1.972656 12.25 1.75 12.25 Z M 1.75 12.25 "/></g></svg></button>',
-    infinite: false, // отключение кнопки дизейбелд
-  });
+  $('.shop__filter-btn').on('click', function () {
+    $('.shop__filters').slideToggle();
+  })
 
-  // табы
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  })
+
+  $('.blog-page__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="7px" height="14px" viewBox="0 0 7 14" version="1.1"><g><path d="M 0.867188 6.535156 L 4.585938 2.816406 C 4.84375 2.558594 5.257812 2.558594 5.511719 2.816406 L 6.128906 3.433594 C 6.386719 3.691406 6.386719 4.105469 6.128906 4.359375 L 3.496094 7 L 6.132812 9.636719 C 6.390625 9.894531 6.390625 10.308594 6.132812 10.5625 L 5.515625 11.183594 C 5.257812 11.441406 4.84375 11.441406 4.589844 11.183594 L 0.871094 7.464844 C 0.609375 7.207031 0.609375 6.792969 0.867188 6.535156 Z M 0.867188 6.535156 "/></g></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="7px" height="14px" viewBox="0 0 7 14" version="1.1"><g id="surface1"><path d="M 6.132812 7.464844 L 2.414062 11.183594 C 2.15625 11.441406 1.742188 11.441406 1.488281 11.183594 L 0.871094 10.566406 C 0.613281 10.308594 0.613281 9.894531 0.871094 9.640625 L 3.503906 7.003906 L 0.871094 4.367188 C 0.613281 4.109375 0.613281 3.695312 0.871094 3.441406 L 1.484375 2.816406 C 1.742188 2.558594 2.15625 2.558594 2.410156 2.816406 L 6.128906 6.535156 C 6.390625 6.792969 6.390625 7.207031 6.132812 7.464844 Z M 6.132812 7.464844 "/></g></svg></button>',
+    infinite: false,
+  })
+
   $('.product-tabs__top-item').on('click', function (e) {
+
     e.preventDefault();
+
     $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
     $(this).addClass('product-tabs__top-item--active');
 
     $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
     $($(this).attr('href')).addClass('product-tabs__content-item--active');
 
-  });
+  })
 
-
-  $('.product-slide__trumb').slick({
+  $('.product-slide__thumb').slick({
     asNavFor: '.product-slide__big',
+    focusOnSelect: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    focusOnSelect: true,
     vertical: true,
     draggable: false,
+
   });
   $('.product-slide__big').slick({
-    asNavFor: '.product-slide__trumb',
+    asNavFor: '.product-slide__thumb',
     draggable: false,
     arrows: false,
+    fade: true,
+    responsive: [
+      {
+        breakpoint: 1051,
+        settings:{
+          draggable: true,
+        }
+      }
+    ]
   });
-
 
   $('.shop-content__filter-btn').on('click', function () {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
     $(this).addClass('shop-content__filter-btn--active');
-  });
+  })
 
   $('.button-list').on('click', function () {
     $('.product-item').addClass('product-item--list');
-  });
-
+    $('.shop-content__inner').addClass('shop-content__nogrid');
+  })
   $('.button-grid').on('click', function () {
     $('.product-item').removeClass('product-item--list');
-  });
+    $('.shop-content__inner').removeClass('shop-content__nogrid');
+  })
 
   $('.select-style, .product-one__num').styler();
 
-  $(".filter-price__input").ionRangeSlider({
+  $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "$",
     onStart: function (data) {
-          $('.filter-price__from').text(data.from);
-          $('.filter-price__to').text(data.to);
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
     },
     onChange: function (data) {
-          $('.filter-price__from').text(data.from);
-          $('.filter-price__to').text(data.to);
-
-        },
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
   });
 
-
-
- $('.top-slider__inner').slick({
-  dots: true,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  fade: true,
-});
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 2000,
+  });
 
   $(".star").rateYo({
+    readOnly: true,
     starWidth: "17px",
     normalFill: "#ccccce",
-    ratedFill: "#ffc35b"
+    ratedFill: "#ffc35b",
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px"height="16px" viewBox="0 0 18 16" version="1.1"><g id="surface1"><path d="M 8.101562 0.554688 L 6.0625 4.695312 L 1.496094 5.359375 C 0.679688 5.476562 0.351562 6.488281 0.945312 7.066406 L 4.246094 10.285156 L 3.464844 14.832031 C 3.324219 15.652344 4.191406 16.269531 4.914062 15.882812 L 9 13.738281 L 13.085938 15.882812 C 13.808594 16.265625 14.675781 15.652344 14.535156 14.832031 L 13.753906 10.285156 L 17.054688 7.066406 C 17.648438 6.488281 17.320312 5.476562 16.503906 5.359375 L 11.9375 4.695312 L 9.898438 0.554688 C 9.53125 -0.179688 8.472656 -0.191406 8.101562 0.554688 Z M 8.101562 0.554688 " /></g></svg>'
   });
 
-  // таймер
   function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(total / (1000 * 60 * 60 * 24));
-  
-  return {
-    total,
-    days,
-    hours,
-    minutes,
-    seconds
-  };
-}
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
-function initializeClock(id, endtime) {
-  const clock = document.querySelector('.promo__clock');
-  const daysSpan = clock.querySelector('.days');
-  const hoursSpan = clock.querySelector('.hours');
-  const minutesSpan = clock.querySelector('.minutes');
-  const secondsSpan = clock.querySelector('.seconds');
-
-  function updateClock() {
-    const t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
   }
 
-  updateClock();
-  const timeinterval = setInterval(updateClock, 1000);
-}
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector('.promo__clock');
+    const daysSpan = clock.querySelector('.promo__days');
+    const hoursSpan = clock.querySelector('.promo__hours');
+    const minutesSpan = clock.querySelector('.promo__minutes');
+    const secondsSpan = clock.querySelector('.promo__seconds');
 
-const deadline = $('.promo__clock').attr('data-time'); // установка времени установка конечной даты распрадажи
-initializeClock('promo__clock', deadline);
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
 
-})
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const deadline = $('.promo__clock').attr('data-time');
+  initializeClock('promo__clock', deadline);
+
+});
